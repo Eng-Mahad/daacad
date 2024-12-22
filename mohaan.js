acc17f1d290b21ed3e944fc5ae5b423c29077add
Eng-Mahad <wllmohaan536@gmail.com>
2024-12-22 11:25:48 +0300

    mohaan

---
+++ mohaan.js
@@ -0,0 +1,47 @@
+let pionts = parseInt(prompt(" gali pionts"))
+
+if(pionts <= 1){
+    alert(" no except ")
+}
+
+ else if(pionts <= 10){
+    alert(" discoun 10% ")
+}
+
+else if(pionts <= 20){
+    alert("discoun 20%")
+}
+else if (pionts <=30){
+    alert("discoun 30%")
+
+}
+else if( pionts <= 40){
+    alert("discoun 40%")
+    
+}
+else if( pionts <= 50){
+    alert("discoun 50%")
+
+}
+
+else if( pionts <= 60){
+    alert("discoun 60%")
+    
+}
+else if( pionts <= 70){
+    alert("discoun 70%")
+    
+}
+else if( pionts <= 80){
+    alert("discoun 80%")
+    
+}
+else if( pionts <= 90){
+    alert("discoun 90%")
+    
+}
+ 
+else{
+    alert("no discoun")
+}
+
